d05131121737609d923ed45ddaa7a62c38041d20
Shammi Shailaj <shammishailaj@gmail.com>
2022-12-15 23:30:46 +0530

    0.0.89 - Fixed issue with templating utility - Changes to README - Makefile now expects arguments `os=<os-name>` and `arch=<arch-name>`. The valid values for `os-name` and `arch-name` can be found out by running the go command `go tool dist list`. - Hardcoded version upgraded to 0.0.89 in `main.go` - Fixed issue where compilation of generated boilerplat was failing due to non-detection of compile-time ldflags being generated by govvv - Changes to deployments/build_cli.sh for `os=<os-name>` and `arch=<arch-name>` - govvv changes for `app.go.tmpl`

--- main.go
+++ main.go
@@ -206,7 +206,7 @@ func main() {
 		version   bool
 	)
 
-	const VERSION = "0.0.88"
+	const VERSION = "0.0.89"
 
 	flag.BoolVar(&version, "version", false, "Prints version and exits")
 	flag.StringVar(&pathToApp, "path", "", "Specify absolute path to app")
